5436d0527cfb39336b4f06a6f3996def186fb9f2
chen-shaoxing <48317826+chen-shaoxing@users.noreply.github.com>
2019-06-07 14:12:22 +0800

    Add files via upload

---
+++ plotgrid.m
@@ -0,0 +1,17 @@
+clear;clc;
+grid_number=18;%格子数
+grid_length=10;%格子宽度
+wirewide=1;%线宽度
+start_color=4;%第几格开始上色
+number_color=5;%上色格数
+a=ones(grid_length*grid_number+wirewide,grid_length*grid_number+wirewide);
+a(grid_length*(start_color-1)+wirewide:grid_length*(start_color+number_color-1),...
+    grid_length*(start_color-1)+wirewide:grid_length*(start_color+number_color-1))=0.75;
+a(1:wirewide,:)=0;
+a(:,1:wirewide)=0;
+for i=1:grid_number
+    a(i*grid_length+1:i*grid_length+wirewide,:)=0;
+    a(:,i*grid_length+1:i*grid_length+wirewide)=0;
+end
+figure;
+imshow(a);
